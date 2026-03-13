333e044abf8a1e32042ffa39607736d6404d380e
Chabbeyj <80900798+Chabbeyj@users.noreply.github.com>
2026-03-13 19:58:22 +0100

    Finishing, commenting and testing question 3

--- Question_3/01_r_script/01_create_ae_summary_table.R
+++ Question_3/01_r_script/01_create_ae_summary_table.R
@@ -1,49 +1,59 @@
-#Packages
+#Question 1: TLG - Adverse Events Reporting
+
+#Objective: Create outputs for adverse events summary using the ADAE dataset and {gtsummary}.
+
+#Start log file
+log_file <- paste0("03_logs/TLG_table_log_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".txt")
+sink(log_file, split = TRUE)
+
+cat("Script started at:", format(Sys.time()), "\n")
+cat("================================\n")
+
+#Load necessary packages
+cat("Loading necessary packages...\n")
 library(pharmaverseadam)
 library(gtsummary)
 library(dplyr)
 library(gt)
+cat("Packages loaded successfully...\n")
 
-#Imput data
+#Loading imput data
+cat("Loading adam data...\n")
 adae <- pharmaverseadam::adae
 adsl <- pharmaverseadam::adsl
+cat("Adam data loaded successfully...\n")
 
 
-#Create a summary table of treatment-emergent adverse events (TEAEs).
-#- Treatment-emergent AE records will have TRTEMFL == "Y" in pharmaverseadam::adae
-#- Rows: AETERM or AESOC
-#- Columns: Treatment groups (ACTARM)
-#- Cell values: Count (n) and percentage (%)
-#- Include total column with all subjects
-#-                                Sort by descending frequency
-
-#Preprocessing
+#Treatment-emergent AE records will have TRTEMFL == "Y" in pharmaverseadam::adae
 adae <- adae |>
   filter(
-    # Treatment Emergent Analysis Flag
     TRTEMFL == "Y"
-    
   )
 
-#Table
+#Create table
 tbl <- adae |>
   tbl_hierarchical(
-    variables = c(AETERM, AESOC),
-    by = ACTARM,
+    variables = c(AETERM, AESOC), #Rows: AETERM or AESOC
+    by = ACTARM,#Columns: Treatment groups (ACTARM)
     id = USUBJID,
     denominator = adsl,
-    #overall_row = TRUE,
-    #label = "..ard_hierarchical_overall.." ~ "Any SAE"
   ) |>
+  #Include total column with all subjects
   add_overall() 
 
 #Sort by descending frequency
 tbl <- sort_hierarchical(tbl)
+cat("Table created successfully...\n")
 
-tbl
 
 #Save the data
+cat("Saving Table...\n")
 tbl %>%
   as_gt() %>%
   gt::gtsave("02_output/TEAEs_table.html")
 
+#End of log file
+cat("================================\n")
+cat("Script completed at:", format(Sys.time()), "\n")
+
+sink()
--- Question_3/01_r_script/02_create_visualizations.R
+++ Question_3/01_r_script/02_create_visualizations.R
@@ -1,33 +1,43 @@
-#Packages
+#Question 1: TLG - Adverse Events Reporting
+
+#Objective: Create outputs for adverse events summary using the ADAE dataset and {gtsummary}.
+
+#Start log file
+log_file <- paste0("03_logs/TLG_visualization_log_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".txt")
+sink(log_file, split = TRUE)
+
+cat("Script started at:", format(Sys.time()), "\n")
+cat("================================\n")
+
+#Load necessary packages
+cat("Loading necessary packages...\n")
 library(pharmaverseadam)
 library(gtsummary)
 library(dplyr)
 library(gt)
 library(ggplot2)
+cat("Packages loaded successfully...\n")
 
-#Imput data
+#Loading imput data
+cat("Loading adam data...\n")
 adae <- pharmaverseadam::adae
 adsl <- pharmaverseadam::adsl
+cat("Adam data loaded successfully...\n")
 
 #AE severity (captured by AESEV) by treatment (ACTARM)
-
 #Barplot
+cat("Plotting AE severity barplots...\n")
 AE_severity_plot <- ggplot(adae, aes(ACTARM)) +
   geom_bar(aes(fill = AESEV)) +
   ylab("Count of AEs") +
   xlab("Treatment Arm")
 AE_severity_plot
 
-#Heatmap
-
-
 #Top 10 most frequent AEs (AETERM)
-
 #Deriving n_subjects from ADSL
 n_subjects <- adsl %>% 
   filter(SAFFL == "Y") %>%
   nrow()
-
 
 #Calculate frequencies and CIs:
 ae_freq <- adae %>%
@@ -42,8 +52,8 @@ ae_freq <- adae %>%
   arrange(desc(pct)) %>%
   slice_head(n = 10)
 
-
 #Plot
+cat("Plotting CI AE plot...\n")
 CI_AE_plot <-  ggplot(ae_freq %>% mutate(AETERM = reorder(AETERM, pct)), aes(x = pct, y = AETERM)) +
   geom_point(size = 3) +
   geom_errorbarh(
@@ -61,10 +71,12 @@ CI_AE_plot <-  ggplot(ae_freq %>% mutate(AETERM = reorder(AETERM, pct)), aes(x =
 CI_AE_plot
 
 #Save the output
+cat("Saving plots...\n")
 ggsave("02_output/AE_severity_plot.png", AE_severity_plot)
 ggsave("02_output/CI_AE_plot.png", CI_AE_plot)
 
+#End of log file
+cat("================================\n")
+cat("Script completed at:", format(Sys.time()), "\n")
 
-
-
-
+sink()
